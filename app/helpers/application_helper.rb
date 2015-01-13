48c1e954327393ccc9d4d78aff6dc0225f535c72
Joe Wanat <wanat.joe@gmail.com>
2015-01-12 21:15:23 -0600

    minor changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title based on page.
+  def full_title(page_title = '')
+    base_title = "TweeClo"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
